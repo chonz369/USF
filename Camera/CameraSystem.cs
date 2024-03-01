a7575775499b6a619e159c4330158877171fa3fc
chonz369 <chengchonzz@gmail.com>
2024-03-01 21:38:29 +0800

    Update CameraSystem.cs

--- Camera/CameraSystem.cs
+++ Camera/CameraSystem.cs
@@ -1,4 +1,3 @@
-using Com.LuisPedroFonseca.ProCamera2D;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using UnityEngine;
@@ -22,38 +21,31 @@ public class CameraSystem : MonoBehaviour, GameSystem
     [SerializeField]
     private Vector2Int defaultScreenSize = new Vector2Int(960, 540);
 
-    public UniTask Init(GameEngine gameEngine)
-    {
+    public UniTask Init(GameEngine gameEngine) {
         Observable.EveryLateUpdate().Subscribe(x => CheckScreenSizeChanged()).AddTo(this);
 
         return UniTask.CompletedTask;
     }
 
-    private void LateUpdate()
-    {
+    private void LateUpdate() {
         CheckScreenSizeChanged();
     }
 
-    private void CheckScreenSizeChanged()
-    {
-        if (Screen.width != screenSize.x || Screen.height != screenSize.y)
-        {
+    private void CheckScreenSizeChanged() {
+        if (Screen.width != screenSize.x || Screen.height != screenSize.y) {
             screenSize = new Vector2Int(Screen.width, Screen.height);
 
             UpdateCameraRect();
         }
     }
 
-    private void UpdateCameraRect()
-    {
+    private void UpdateCameraRect() {
         var screenRatio = Screen.width / (float)Screen.height;
         var defaultRatio = defaultScreenSize.x / (float)defaultScreenSize.y;
-        if (screenRatio > defaultRatio)
-        {
+        if (screenRatio > defaultRatio) {
             Vector2 size = new Vector2(1 / (screenRatio / defaultRatio), 1);
             MainCamera.rect = new Rect((1 - size.x) / 2, 0, size.x, size.y);
-        } else
-        {
+        } else {
             Vector2 size = new Vector2(1, 1 / (defaultRatio / screenRatio));
             MainCamera.rect = new Rect(0, (1 - size.y) / 2, size.x, size.y);
         }
